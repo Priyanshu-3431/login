0dcfe558652303d5bb76048f1f7dc95f9feb3b71
Priyanshu-3431 <priyanshu34321@gmail.com>
2026-02-17 19:19:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const userModel = require("./user");
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.post('/register', async (req, res) => {
+  let { email, password, username, name, age } = req.body;
+
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).send("User already registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        email,
+        age,
+        name,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userid: user._id }, "shhh");
+     res.cookie("token",token);
+res.send("User registered successfully");
+    })
+  })
+
+});
+
+
+
+
+
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
